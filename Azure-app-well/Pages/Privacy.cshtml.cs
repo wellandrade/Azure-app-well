3a71627f06e2b4f713d308853458adf335cc0757
welli <wellington.andrade@hotmail.com.br>
2026-05-06 22:44:11 -0300

    Add project files.

---
+++ Azure-app-well/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Azure_app_well.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
